b64810b28bd596559d7d186960807a738a2d57a5
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2019-10-13 15:52:32 +0200

    Clean-up, added comments

--- app/src/main/java/com/schneiderchristian/simplelistview/adapters/ShoppingListAdapter.java
+++ app/src/main/java/com/schneiderchristian/simplelistview/adapters/ShoppingListAdapter.java
@@ -1,7 +1,7 @@
 package com.schneiderchristian.simplelistview.adapters;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,54 +18,66 @@ import com.schneiderchristian.simplelistview.models.ShoppingItem;
 import java.util.ArrayList;
 
 public class ShoppingListAdapter extends ArrayAdapter {
-    private final Activity myContext;
-    private final ArrayList<ShoppingItem> myShoppingList;
+    private final Activity activity;
+    private final ArrayList<ShoppingItem> shoppingList;
 
+    //ViewHolder als verschachtelte Klasse (nested class)
     static class ViewHolder {
-        public ImageView imgShoppingItem;
-        public TextView txtShoppingItemTitle;
-        public TextView txtShoppingItemDescription;
-        public TextView txtShoppingItemAmount;
+        ImageView imgShoppingItem;
+        TextView txtShoppingItemTitle;
+        TextView txtShoppingItemDescription;
+        TextView txtShoppingItemAmount;
     }
 
-    public ShoppingListAdapter(Activity context, ArrayList<ShoppingItem> shoppingList) {
-        super(context, R.layout.shopping_item, shoppingList);
-        this.myContext = context;
-        this.myShoppingList = shoppingList;
+    public ShoppingListAdapter(Activity activity, ArrayList<ShoppingItem> shoppingList) {
+        //Übergabe der Activity, des Layouts und der Daten an einen übergeordneten Konstruktor von ArrayAdapter
+        super(activity, R.layout.shopping_item, shoppingList);
+        this.activity = activity;
+        this.shoppingList = shoppingList;
     }
 
+    @SuppressLint("InflateParams")
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //Zur besseren Übersicht. Natürlich könnte man auch direkt auf convertView arbeiten
         View rowView = convertView;
 
+        //Zuweisen eines Views für rowView, falls dieser noch nicht besteht
         if(rowView == null) {
-            LayoutInflater layoutInflater = myContext.getLayoutInflater();
+            LayoutInflater layoutInflater = activity.getLayoutInflater();
             rowView = layoutInflater.inflate(R.layout.shopping_item, null);
             ViewHolder viewHolder = new ViewHolder();
             viewHolder.imgShoppingItem = rowView.findViewById(R.id.imgShoppingItem);
             viewHolder.txtShoppingItemTitle = rowView.findViewById(R.id.txtShoppingItemTitle);
             viewHolder.txtShoppingItemDescription = rowView.findViewById(R.id.txtShoppingItemDescription);
             viewHolder.txtShoppingItemAmount = rowView.findViewById(R.id.txtShoppingItemAmount);
+            //Versehen des rowViews mit einem Tag zur Identifizierung im Speicher
             rowView.setTag(viewHolder);
         }
-
+        //holen des Views aus dem Speicher und befüllen mit Daten
         ViewHolder viewHolder = (ViewHolder) rowView.getTag();
-        viewHolder.txtShoppingItemTitle.setText(myShoppingList.get(position).getName());
-        viewHolder.txtShoppingItemDescription.setText(myShoppingList.get(position).getDescription());
-        viewHolder.txtShoppingItemAmount.setText(myShoppingList.get(position).getAmount());
-        String itemSource = myShoppingList.get(position).getSource();
-        int itemSourceRes = 0;
-        if(itemSource.equals("butcher")) {
-            itemSourceRes = R.drawable.butcher;
-        } else if(itemSource.equals("drugstore")) {
-            itemSourceRes = R.drawable.drugstore;
-        } else if (itemSource.equals("supermarket")) {
-            itemSourceRes = R.drawable.supermarket;
-        } else {
-            itemSourceRes = R.drawable.shopping_basket;
+        viewHolder.txtShoppingItemTitle.setText(shoppingList.get(position).getName());
+        viewHolder.txtShoppingItemDescription.setText(shoppingList.get(position).getDescription());
+        viewHolder.txtShoppingItemAmount.setText(shoppingList.get(position).getAmount());
+        String itemSource = shoppingList.get(position).getSource();
+        int itemSourceRes;
+        switch (itemSource) {
+            case "butcher":
+                itemSourceRes = R.drawable.butcher;
+                break;
+            case "drugstore":
+                itemSourceRes = R.drawable.drugstore;
+                break;
+            case "supermarket":
+                itemSourceRes = R.drawable.supermarket;
+                break;
+            default:
+                itemSourceRes = R.drawable.shopping_basket;
+                break;
         }
         viewHolder.imgShoppingItem.setImageResource(itemSourceRes);
+        //Übergabe des Views als Listenelement an den ListView
         return rowView;
     }
 }
